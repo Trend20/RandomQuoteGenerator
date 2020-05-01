a79853dfe6464146b6cfed3f4a6647dd3ad34811
Trend20 <enockomondi305@gmail.com>
2020-05-01 11:28:08 -0400

    third commit

--- app.js
+++ app.js
@@ -3,7 +3,33 @@ const authorName = document.querySelector('#author');
 const quoteBtn = document.querySelector('#new-quote');
 
 
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
 
+function getQuote(){
+    fetch(endpoint)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      displayQuote(data.message);
+    })
+    .catch(function () {
+      console.log("An error occurred");
+    });
+}
+
+function displayQuote(quote) {
+  const quoteText = document.querySelector('.quote-text');
+  quoteText.textContent = quote;
+
+  const tweetButton = document.querySelector('.tweet');
+  tweetButton.setAttribute('href', `https://twitter.com/share?text=${quote}`);
+}
+
+const newQuoteButton = document.querySelector('.new-quote');
+newQuoteButton.addEventListener('click', getQuote);
+
+getQuote();
 // let quotes = [
 //     'Loving',
 //     'Cooking',

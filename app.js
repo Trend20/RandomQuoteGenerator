16636c250dfcb3685da832b0fbd5ed32eb5bbbcd
Trend20 <enockomondi305@gmail.com>
2021-01-25 10:39:54 +0300

    adding the api endpoint

--- app.js
+++ app.js
@@ -5,46 +5,46 @@ const quoteBtn = document.querySelector('#new-quote');
 
 const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
 
-function getQuote(){
+function getQuote() {
     fetch(endpoint)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      displayQuote(data.message);
-    })
-    .catch(function () {
-      console.log("An error occurred");
-    });
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            displayQuote(data.message);
+        })
+        .catch(function() {
+            console.log("An error occurred");
+        });
 }
 
 function displayQuote(quote) {
-  const quoteText = document.querySelector('.quote-text');
-  quoteText.textContent = quote;
+    const quoteText = document.querySelector('.quote-text');
+    quoteText.textContent = quote;
 
-  const tweetButton = document.querySelector('.tweet');
-  tweetButton.setAttribute('href', `https://twitter.com/share?text=${quote}`);
+    const tweetButton = document.querySelector('.tweet');
+    tweetButton.setAttribute('href', `https://twitter.com/share?text=${quote}`);
 }
 
-const newQuoteButton = document.querySelector('.new-quote');
+const newQuoteButton = document.querySelector('#new-quote');
 newQuoteButton.addEventListener('click', getQuote);
 
 getQuote();
-// let quotes = [
-//     'Loving',
-//     'Cooking',
-//     'Dancing',
-//     'Playing',
-//     'Eating',
-//     'Drinking',
-// ];
+let quotes = [
+    'Loving',
+    'Cooking',
+    'Dancing',
+    'Playing',
+    'Eating',
+    'Drinking',
+];
 
-// quoteBtn.addEventListener('click', newQuote);
+quoteBtn.addEventListener('click', newQuote);
 
-// function newQuote(){
-//     let randomNumber = Math.floor(Math.random() * (quotes.length));
-//     let realQuote = quoteText.innerHTML = (quotes[randomNumber]);
-//     let quotationMark = document.createElement('i');
-//     quotationMark.innerHTML = 'fas fa-quote-left';
-//     insertAfter(quotationMark, realQuote);
-// }
+function newQuote() {
+    let randomNumber = Math.floor(Math.random() * (quotes.length));
+    let realQuote = quoteText.innerHTML = (quotes[randomNumber]);
+    let quotationMark = document.createElement('i');
+    quotationMark.innerHTML = 'fas fa-quote-left';
+    insertAfter(quotationMark, realQuote);
+}
